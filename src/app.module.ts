1602112c87b8d59123a88173bb59ed9d8da3218e
manu05X <k.manu00005@gmail.com>
2022-11-16 23:16:54 +0530

    dependencies commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { StudentModule } from './student/student.module';
-import { StreamModule } from './stream/stream.module';
 import { SubjectModule } from './subject/subject.module';
-import { MarksModule } from './marks/marks.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [StudentModule, StreamModule, SubjectModule, MarksModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // shouldn't be used in production - may lose data
+    }),
+    StudentModule,
+    SubjectModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,4 +6,8 @@ import { UserService } from './user.service';
   controllers: [UserController],
   providers: [UserService]
 })
-export class UserModule {}
+export class UserModule {
+  constructor(){
+    console.log('UserModule');
+  }
+}
